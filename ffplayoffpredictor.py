3956b51805938a2a2ab4ba4784c016cf54feea25
Duncan Harvey <duncanpharvey@gmail.com>
2019-11-21 10:15:40 -0800

    cleans up code formatting and visualization formatting

--- ffplayoffpredictor.py
+++ ffplayoffpredictor.py
@@ -11,7 +11,7 @@ class Team:
         self.points = points
         self.ranksummary = ranksummary
 
-chubba = Team("chubba chubba choo choo", 9, 1079.1, ranksummary.copy())
+chubba = Team("chubba chubba choo choo", 9, 1078.4, ranksummary.copy())
 golladays = Team("happy golladays", 8, 1167.76, ranksummary.copy())
 bortles = Team("bortles' chortles", 7, 1168.58, ranksummary.copy())
 job = Team("do your job", 7, 1120.26, ranksummary.copy())
@@ -48,6 +48,7 @@ game16 = [obj, juju]
 game17 = [apple, allstars]
 game18 = [charm, luck]
 
+#find all possible scenarios
 results = list(itertools.product(game1, game2, game3, game4, game5, game6, \
                                  game7, game8, game9, game10, game11, game12, \
                                  game13, game14, game15, game16, game17, game18))
@@ -55,8 +56,9 @@ results = list(itertools.product(game1, game2, game3, game4, game5, game6, \
 count = 0
 totalscenarios = 2 ** 18
 print("total number of scenarios: ", totalscenarios)
-print("evaluating scenarios...", end = "")
+print("evaluating scenarios...", end = '')
 
+#determine number of wins for each team at the end of each scenario
 for result in results:
     for team in teams:
         team.wins = team.currentwins #reset wins for each team for each result
@@ -70,16 +72,14 @@ for result in results:
         teams[i].ranksummary[i + 1] += 1
 
     if count % 100000 == 0:
-        print(count, end = "")
+        print(count, end = '')
         
     if count % 10000 == 0:
-        print(".", end = "")
+        print('.', end = '')
     
     count += 1
 
-print("\n")
-
-#determine playoff odds- teams make playoffs if they are one of the top 4 ranked teams
+#determine playoff odds based off of scenario summary- teams make playoffs if they are one of the top 4 ranked teams
 for team in teams:
     playoffodds = 0
     for rank in range(1, 5):
@@ -87,17 +87,21 @@ for team in teams:
     team.playoffodds = round(playoffodds * 100, 2)
 
     expectedrank = 0
-    for rank in range(1, 13):
+    for rank in team.ranksummary:
         expectedrank += rank * (team.ranksummary[rank] / totalscenarios)
-    team.expectedrank = expectedrank
+    team.expectedrank = round(expectedrank, 2)
 
 teams.sort(key = lambda x: x.expectedrank)
 
-for team in teams:
-    for key in team.ranksummary:
-        team.ranksummary[key] = round((team.ranksummary[key] / totalscenarios) * 100, 2) #convert to percentage
-    #print(team.name, str(team.playoffodds) + "%", team.expectedrank, team.ranksummary)
+print('\n')
 
+#round and display raw rank data
+for team in teams:
+    for rank in team.ranksummary:
+        team.ranksummary[rank] = round((team.ranksummary[rank] / totalscenarios) * 100, 2)
+    print(team.name, '|', str(team.playoffodds) + '%', '|', team.expectedrank, '|', team.ranksummary)
+
+#save data to use in pandas
 playoffodds = pd.DataFrame({'rank' : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12],
                             chubba.name : list(chubba.ranksummary.values()),
                             golladays.name : list(golladays.ranksummary.values()),
@@ -112,17 +116,30 @@ playoffodds = pd.DataFrame({'rank' : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12],
                             charm.name : list(charm.ranksummary.values()),
                             ships.name : list(ships.ranksummary.values())})
 
+#initialize plot
 fig, axes = plt.subplots(figsize = (9, 9), nrows = 3, ncols = 4)
 playoffodds.set_index('rank', drop = True, inplace = True)
 
+colors = []
+playoffcolors = ['g'] * 4
+othercolors = ['b'] * 7
+lastplacecolor = 'r'
+
+colors.extend(playoffcolors)
+colors.extend(othercolors)
+colors.append(lastplacecolor)
+
+#generate subplots for each team
 for x in range(0, 3):
     for y in range(0, 4):
         teamname = teams[(4 * x) + y].name
-        playoffodds[teamname].plot(ax = axes[x,y], kind = 'bar', color=['C0', 'C0', 'C0', 'C0', 'C1', 'C1', 'C1', 'C1', 'C1', 'C1', 'C1', 'C3'])
+        playoffodds[teamname].plot(ax = axes[x,y], kind = 'bar', color = colors)
         axes[x, y].set_ylim(0, 60)
         axes[x, y].set_title(teamname)
-        #axes[x, y].set_ylabel('% chance of outcome')
         axes[x, y].set_xlabel('')
-        
-plt.tight_layout()
+
+fig.text(0.5,0.04, "rank", ha = "center", va = "center")
+fig.text(0.05,0.5, "% chance of outcome", ha = "center", va = "center", rotation = 90)
+
+plt.tight_layout(pad = 4.0, w_pad = 0.0, h_pad = 1.0)
 plt.show()
